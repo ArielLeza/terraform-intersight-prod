7e89cac6583e7dc4c2b169724a837902f1abe1d6
ArielLeza <ariel.leza@gmail.com>
2021-04-29 23:11:19 +0300

    first intersight api call

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 
 provider "intersight" {
-  # Configuration options
+  apikey = "5bfbef9e356c36396a62b69c/5cddc68d7564612d30e3be85/60896ba27564612d3016595c",
+  endpoint = "https://www.intersight.com/api/v1",
+  secretkey = "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgrIvVbZCC+HVU0Bjr
+RY3KMGvUnHQJG/mZz9Te8EGbrnahRANCAAR81QfUG4p7vE/A3divG2l0rHJDbNEu
+zZdVOabiRrWe8UEmeHCbHfao+06zM7AeyJJQLo/Dfw52A8rTcwuWivf6",
 }
