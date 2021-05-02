9b9d6e80f56ff55a0882a797ca7c24569835d2a1
ArielLeza <ariel.leza@gmail.com>
2021-05-02 19:27:53 +0300

    default parameters

--- variables.tf
+++ variables.tf
@@ -1,9 +1,13 @@
 variable "api_key" {
   type = string
   description = "API Key Id from Intersight"
+  default = ""
+
 }
 
 variable "secret_key" {
   type = string
   description = "API Key Id from Intersight"
+  default = ""
+
 }
