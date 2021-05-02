da163e25fc2dd46c70104890bc4e736746675e35
ArielLeza <ariel.leza@gmail.com>
2021-05-02 18:48:00 +0300

    text

--- main.tf
+++ main.tf
@@ -11,5 +11,31 @@ terraform {
 provider "intersight" {
   apikey =  var.api_key
   endpoint = "https://www.intersight.com"
-  secretkey = "https://github.com/ArielLeza/terraform-intersight-prod/blob/754428741356fc6df80339f86760129cb0439f7d/SecretKey.pem"
+  secretkey = "-----BEGIN RSA PRIVATE KEY-----
+  MIIEowIBAAKCAQEAvPpeWcBfWCB2ItQCxYjqEV7Guv3c7EZ/FC9W1Sw9KI1D6Pmg
+  sOGTwOeEw55Y1JDW531J/YfqpLqedAo/PFjE7sEIzGcdfM98nyWYoXbcaijIcBWc
+  Bczg41suZCHk26CJEySgcMIzLketLSENuUt0f10+fBwuRR/8lXfkxhXuCwHSJjiw
+  hjKG8XLERCkUQwk4FKpV+b0a+aCJt2T5Th/wlwZwDyhfQ4gTGXZr432KdDSoaDRx
+  tf81Br2cp7kQj58vF/clqNKSzPDlnw3JhS2BoYB/VF0Ba0KS634nPkmw+mL5tMp3
+  zwaDLB8zyBv866nAYT4me6kY3vhwSPOF+L5rjwIDAQABAoIBAB/SmWjLuEKC1OHw
+  ibbx41iheR2r+jPfey7O+fRTysi1YSYuQx0GH3dwhS7bz2GcMlQxCPCknqK2ulAY
+  GaXz3NXHs8+X3aNS3U1/a0BlX6CQ1Y/qU6e2ZHhktl8uRfemBO8wT8QtL6kvcL4U
+  vtbhGfEIpqkHpdMGG+afleq1bqxzi+Hn+g555YC2YzRXnmRt5HW25ioaW+WG9ygt
+  tD5DxLlyHiW4/twrci5pSutDBHCvrAIkz3/HNXTpi1s6zrKBGb90giwR4e3F5167
+  CYab26zLIUzp3X1Aayd0CByZ82+WfOskpstG5NM5Lc9Y4D68ftz/x+s8aZR45qSH
+  aSQDYYECgYEA0ZfLMMVgZbgkICYL95Cs9iWVGlY5CrrKo8Uhk5Noo+wyFmUJS15M
+  8hcvp+mIAKgOgdMs1wAevb4CYlCBZ2OXQIfLHeaxOMMgQAcQ+xgTYpMgSYP1w1lR
+  nTxCm2QHWBgUkwYvArVstzeCqCvFqaImudU+GUwhfnQpHh7Q8zoGW7ECgYEA5tIS
+  qUPWZgLvuL7w+Yd8byFjdJY8MyLXFq3zrvBwhrlUxvi7aSPPEe8sqVVnBiOlEUqw
+  KSAOLnF89h/F86ZUSjldE7bm2XF2Vp32fanAttdJtQY9PzQ6cBLnrT3Fo+nHA0Ti
+  29EwBFIv9dcCXMKsviNeHdFAC5jeGDtwSkqhSz8CgYBFKWvZaihHKJ5zJVrplIoe
+  p4QuLqmgCsd3h0NvKUNsZDKbBTgt8K4/bLtDE2LXpJ005fRz9gwu3JZ4zswEy+5L
+  UwQJl+h/LJRlzG8kDGvJKXeMCsl8burpkcsA798kiL2xTmcQ48E+LEknKVqOgX15
+  8Q4O1vpuefJfiBe3Szxx4QKBgQCjv3JHrH2EHx8rhCH2E9z+BcTFp/MLH/NQ/pT6
+  jU529TGkej6SBzpWccPQWWreGxl2W/L/KeOFpTNOdBn9kQkJIwrZ+o5owip+pk+A
+  tJWn4LFEgCmhFg9DBNZgu4rjjuyFcDA0W4Hh/uBRWngtkFn2Bac2JgV2dLtSm5A8
+  1Bb7BwKBgE1ifTXcZ/YmFehdC9EWY2Qgwrw+TAEFI5hLT1vf5aWjOTgc+VnzV/aN
+  LU1Cl9A+ZW0fSGS/GkT4m1ZEJEMiJsnmjO4u+gSRab67gZwXmaZIv5qd9eVxP6Ei
+  sUe7dozq4PM0a7hskyihFzeYkSdrnEmu3TDjeHKv6KaEhJjwc6tH
+  -----END RSA PRIVATE KEY-----"
 }
