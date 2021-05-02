754428741356fc6df80339f86760129cb0439f7d
ArielLeza <ariel.leza@gmail.com>
2021-05-02 16:54:02 +0300

    API key via URL

--- main.tf
+++ main.tf
@@ -10,5 +10,5 @@ terraform {
 provider "intersight" {
   apikey = "5bfbef9e356c36396a62b69c/5cddc68d7564612d30e3be85/60896ba27564612d3016595c"
   endpoint = "https://www.intersight.com"
-  secretkey = "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgrIvVbZCC+HVU0BjrRY3KMGvUnHQJG/mZz9Te8EGbrnahRANCAAR81QfUG4p7vE/A3divG2l0rHJDbNEuzZdVOabiRrWe8UEmeHCbHfao+06zM7AeyJJQLo/Dfw52A8rTcwuWivf6"
+  secretkey = "https://github.com/ArielLeza/terraform-intersight-prod/blob/a18a0bdb56a84f0444b73f24ae2ec10ca099814a/SecretKey.txt"
 }
