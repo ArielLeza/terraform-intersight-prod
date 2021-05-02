9f68daeab3754ea142f4f18b9690bebedb655c5a
ArielLeza <ariel.leza@gmail.com>
2021-05-02 16:43:51 +0300

    Added orrect API target

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 provider "intersight" {
   apikey = "5bfbef9e356c36396a62b69c/5cddc68d7564612d30e3be85/60896ba27564612d3016595c",
-  endpoint = "https://www.intersight.com/api/v1",
+  endpoint = "https://www.intersight.com",
   secretkey = "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgrIvVbZCC+HVU0Bjr
 RY3KMGvUnHQJG/mZz9Te8EGbrnahRANCAAR81QfUG4p7vE/A3divG2l0rHJDbNEu
 zZdVOabiRrWe8UEmeHCbHfao+06zM7AeyJJQLo/Dfw52A8rTcwuWivf6",
