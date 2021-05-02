2dd530593789254041298573c9429aef7ad52dc1
ArielLeza <ariel.leza@gmail.com>
2021-05-02 16:45:46 +0300

    v3

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 provider "intersight" {
-  apikey = "5bfbef9e356c36396a62b69c/5cddc68d7564612d30e3be85/60896ba27564612d3016595c",
-  endpoint = "https://www.intersight.com",
-  secretkey = "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgrIvVbZCC+HVU0BjrRY3KMGvUnHQJG/mZz9Te8EGbrnahRANCAAR81QfUG4p7vE/A3divG2l0rHJDbNEuzZdVOabiRrWe8UEmeHCbHfao+06zM7AeyJJQLo/Dfw52A8rTcwuWivf6",
+  apikey = "5bfbef9e356c36396a62b69c/5cddc68d7564612d30e3be85/60896ba27564612d3016595c"
+  endpoint = "https://www.intersight.com"
+  secretkey = "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgrIvVbZCC+HVU0BjrRY3KMGvUnHQJG/mZz9Te8EGbrnahRANCAAR81QfUG4p7vE/A3divG2l0rHJDbNEuzZdVOabiRrWe8UEmeHCbHfao+06zM7AeyJJQLo/Dfw52A8rTcwuWivf6"
 }
