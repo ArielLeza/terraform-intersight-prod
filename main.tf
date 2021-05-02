f8bba951a7758776ed4e2f93072f7fd77be19e42
ArielLeza <ariel.leza@gmail.com>
2021-05-02 17:03:55 +0300

    fix pem

--- main.tf
+++ main.tf
@@ -10,5 +10,5 @@ terraform {
 provider "intersight" {
   apikey = "5bfbef9e356c36396a62b69c/5cddc68d7564612d30e3be85/60896ba27564612d3016595c"
   endpoint = "https://www.intersight.com"
-  secretkey = "https://github.com/ArielLeza/terraform-intersight-prod/blob/754428741356fc6df80339f86760129cb0439f7d/SecretKey.txt"
+  secretkey = "https://github.com/ArielLeza/terraform-intersight-prod/blob/754428741356fc6df80339f86760129cb0439f7d/SecretKey.pem"
 }
