ae8e1c8e9126f372032950a84527c2565051377d
ArielLeza <ariel.leza@gmail.com>
2021-05-02 18:08:23 +0300

    t

--- variables.tf
+++ variables.tf
@@ -3,27 +3,27 @@ variable "api_key" {
   description = "API Key Id from Intersight"
 }
 
-variable "encryption_key" {
+#variable "encryption_key" {
   type = string
   description = "Encryption key for IPMIOverLan Policy"
-}
+#}
 
-variable "auth_password" {
+#variable "auth_password" {
   type = string
   description = "Auth Password for SNMP Policy"
-}
+#}
 
-variable "privacy_password" {
+#variable "privacy_password" {
   type = string
   description = "Privacy Password for SNMP Policy"
-}
+#}
 
-variable "base_properties_password_1" {
+#variable "base_properties_password_1" {
   type = string
   description = "Password field in Base properties in LDAP policy"
-}
+#}
 
-variable "organization" {
+#variable "organization" {
   type = string
   description = "Organization moid"
-}
+#}
