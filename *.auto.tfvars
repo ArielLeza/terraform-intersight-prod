792e17ccb2871cf20cb8d23e5b50d8fdaad3b4f2
ArielLeza <ariel.leza@gmail.com>
2021-05-03 00:50:31 +0300

    complete

---
+++ *.auto.tfvars
@@ -0,0 +1,17 @@
+# Required Varilables
+api_key      = "5bfbef9e356c36396a62b69c/5cddc68d7564612d30e3be85/608ec8e57564612d3034c14a"
+vc_password  = "C!sco12345"
+ssh_user     = "iksadmin"
+ssh_key      = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDVNXLicOb1aMaVMGBuutCh8b50WNVi1OCMLct7Rrkox aleza@cisco.com"
+# Optional Variables
+tags = [
+  {
+    "key" : "Provisioned"
+    "value" : "TFC"
+  },
+  {
+    "key" : "Owner"
+    "value" : "Ariel"
+  }
+]
+organization = "default" # Change this if a different org is required.  Default org is set to "default"
--- main.tf
+++ main.tf
@@ -1,14 +1,46 @@
-terraform {
-  required_providers {
-    intersight = {
-      source = "CiscoDevNet/intersight"
-      version = "1.0.7"
-    }
-  }
+provider "intersight" {
+  apikey    = var.api_key
+  secretkey = var.secretkey
+  endpoint  = var.endpoint
 }
 
-provider "intersight" {
-  apikey    = var.apikey
-  secretkey = var.secretkey
-  endpoint = "https://intersight.com"
+module "terraform-intersight-iks" {
+  source = "terraform-cisco-modules/iks/intersight//"
+  # Infra Config Policy Information
+  cluster_name = "test0920"
+  # cluster_action   = "Deploy"
+  vc_target_name   = "192.168.31.2"
+  vc_portgroup     = ["MGMT-30"]
+  vc_datastore     = "DS-1"
+  vc_cluster       = ""
+  vc_resource_pool = ""
+  vc_password      = var.vc_password
+
+  # IP Pool Information
+  ip_starting_address = "10.139.120.220"
+  ip_pool_size        = "20"
+  ip_netmask          = "255.255.255.0"
+  ip_gateway          = "10.139.120.1"
+  ip_primary_dns      = "10.101.128.15"
+  ip_secondary_dns    = "10.101.128.16"
+
+  # Network Configuration Settings
+  # pod_cidr = "100.65.0.0/16"
+  # service_cidr = "100.64.0.0/24"
+  # cni = "Calico"
+  domain_name         = "rich.ciscolabs.com"
+  timezone            = "America/New_York"
+  unsigned_registries = ["10.101.128.128"]
+  # root_ca_registries  = [""]
+
+  # Cluster information
+  ssh_user       = var.ssh_user
+  ssh_key        = var.ssh_key
+  worker_size    = "small"
+  worker_count   = 4
+  master_count   = 1
+  load_balancers = 3
+  # Organization and Tag
+  organization = var.organization
+  tags         = var.tags
 }
