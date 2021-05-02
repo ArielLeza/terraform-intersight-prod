8406227ff7af53bf48664755dea2c99151989709
ArielLeza <ariel.leza@gmail.com>
2021-05-02 18:06:14 +0300

    test

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "intersight" {
-  apikey = "5bfbef9e356c36396a62b69c/5cddc68d7564612d30e3be85/60896ba27564612d3016595c"
+  apikey =  var.api_key
   endpoint = "https://www.intersight.com"
   secretkey = "https://github.com/ArielLeza/terraform-intersight-prod/blob/754428741356fc6df80339f86760129cb0439f7d/SecretKey.pem"
 }
