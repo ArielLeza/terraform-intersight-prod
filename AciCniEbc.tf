d632b4026773b4e0289f7566cc618be2b909bfb8
ArielLeza <ariel.leza@gmail.com>
2021-05-04 11:16:49 +0300

    ddemo for eyal

--- AciCniEbc.tf
+++ AciCniEbc.tf
@@ -11,3 +11,19 @@ resource "intersight_kubernetes_aci_cni_apic" "kubernetes_aci_cni_apic1" {
     object_type = "asset.DeviceRegistrations"
   }
 }
+
+resource "intersight_kubernetes_aci_cni_profile" "kubernetes_aci_cni_profile1" {
+  description           = "kubernetes aci cni profile"
+  name                  = "IKS ACI-CNI for EBC-Cloud"
+  type                  = "instance"
+  node_svc_subnet_start = "10.0.0.0/8"
+  pod_subnet_start      = "10.0.0.0/8"
+  organization {
+    object_type = "organization.Organization"
+    moid        = var.organization
+  }
+  registered_device {
+    moid        = intersight_registered_device.device1.id
+    object_type = "asset.DeviceRegistrations"
+  }
+}
