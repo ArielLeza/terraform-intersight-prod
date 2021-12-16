2552c2244492a175b805bc3ba71b868c2af7ab20
ArielLeza <ariel.leza@gmail.com>
2021-12-16 17:14:46 -0600

    test

--- main.tf
+++ main.tf
@@ -102,25 +102,27 @@ module "terraform-intersight-iks" {
 
 # Addon Profile and Policies (To create new change "createNew" to 'true' and uncomment variables and modify them to meet your needs.)
 # This is an Optional item.  Comment or remove to not use.  Multiple addons can be configured.
-  addons       = {
-    createNew = true
-    addonPolicyName = "IKS-EBC-Prod-TFC-AddonPol"
-    addonName            = "smm"
-    description       = "SMM Policy"
-    upgradeStrategy  = "AlwaysReinstall"
-    installStrategy  = "InstallOnly"
-    releaseVersion = "1.8.1-cisco2-helm3"
-    overrides = yamlencode({"demoApplication":{"enabled":true}})
-  }
-    # {
-    # createNew = true
-    # addonName            = "ccp-monitor"
-    # description       = "monitor Policy"
-    # # upgradeStrategy  = "AlwaysReinstall"
-    # # installStrategy  = "InstallOnly"
-    # releaseVersion = "0.2.61-helm3"
-    # # overrides = yamlencode({"demoApplication":{"enabled":true}})
-    # }
+  addons       = [
+  {
+  createNew = true
+  addonPolicyName = "smm-tf"
+  addonName            = "smm"
+  description       = "SMM Policy"
+  upgradeStrategy  = "AlwaysReinstall"
+  installStrategy  = "InstallOnly"
+  releaseVersion = "1.7.4-cisco4-helm3"
+  overrides = yamlencode({"demoApplication":{"enabled":true}})
+  },
+  # {
+  # createNew = true
+  # addonName            = "ccp-monitor"
+  # description       = "monitor Policy"
+  # # upgradeStrategy  = "AlwaysReinstall"
+  # # installStrategy  = "InstallOnly"
+  # releaseVersion = "0.2.61-helm3"
+  # # overrides = yamlencode({"demoApplication":{"enabled":true}})
+  # }
+  ]
 
 
 # Worker Node Instance Type (To create new change "use_existing" to 'false' and uncomment variables and modify them to meet your needs.)
